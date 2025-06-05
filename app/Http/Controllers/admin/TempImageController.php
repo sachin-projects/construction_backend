00b92c6b0bd810ff31d0024e06c0e58c8e32b757
Sachin <sachingramtekkar@gmail.com>
2025-06-05 16:44:47 +0530

    update

--- app/Http/Controllers/admin/TempImageController.php
+++ app/Http/Controllers/admin/TempImageController.php
@@ -4,8 +4,52 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\TempImage;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class TempImageController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'image' => 'required|mimes:jpg,jpeg,png,pdf,gif',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $image = $request->image;
+
+        $ext = $image->getClientOriginalExtension();
+        $imageName = strtotime(now()).'.'.$ext;
+         
+        //save data in TempImage table
+        $model = new TempImage();
+        $model->name = $imageName;
+        $model->save();
+
+        //save image in public/uploads/temp folder
+        $image->move(public_path('uploads/temp'),$imageName);
+
+        //create small thumbnail images
+        $sourcePath = public_path('uploads/temp/'.$imageName);
+        $destPath = public_path('uploads/temp/thumb/'.$imageName);
+        $manager = new ImageManager(Driver::class);
+        $image = $manager->read($sourcePath);
+        $image->coverDown(300,300);
+        $image->save($destPath);
+
+        return response()->json([
+            'status' => true,
+            'data' => $model,
+            'message' => 'Image uploaded successfully'
+        ]);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\ServiceController;
+use App\Http\Controllers\admin\TempImageController;
 
 
 Route::post('authenticate', [AuthenticationController::class, 'authenticate']);
@@ -17,6 +18,9 @@ Route ::group(['middleware'=>['auth:sanctum']], function(){
     //Services Routes
     Route::post('services', [ServiceController::class, 'store']);
     Route::get('services', [ServiceController::class, 'index']);
+
+    //Temp images
+    Route::post('temp-images', [TempImageController::class, 'store']);
 });
 
 // Route::get('/user', function (Request $request) {
